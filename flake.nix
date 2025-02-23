0236ec8a3ba65c36b700516c02b1fdcfadefcc55
exclusive-and <exclusiveandgate@gmail.com>
2025-02-22 19:58:20 -0500

    Fixed borked flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 
 {
-    description = "yang";
+    description = "hyperion OS configuration";
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-24.11";
+
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
         challengerDeep = {
             url = "github:challenger-deep-theme/vim";
             flake = false;
@@ -18,11 +20,11 @@
         nixosConfigurations = {
             hyperion = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
+
                 modules = with inputs; [
                     ./configuration.nix
-                    ./hyperion
-                    home-manager.nixosModules.home-manager
                 ];
+                
                 specialArgs = {
                     inherit inputs;
                     inherit (inputs) challengerDeep;
