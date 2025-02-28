f1de38caed09c771e2c76fbdfb251d414ca0945b
xand <exclusiveandgate@gmail.com>
2025-02-28 14:44:45 -0500

    Updated ddclient for new domain names

--- services/ddclient/default.nix
+++ services/ddclient/default.nix
@@ -3,14 +3,6 @@
 {
     services.ddclient = {
         enable = true;
-
-        username = "xandgate.com";
-        passwordFile = "/etc/ddclient/xandgate-password";
-
-        domains = [ "@" ];
-
-        protocol = "namecheap";
-        server = "dynamicdns.park-your-domain.com";
-        use = "web, web=https://dynamicdns.park-your-domain.com/getip";
+        configFile = "/etc/ddclient/ddclient.conf";
     };
 }
